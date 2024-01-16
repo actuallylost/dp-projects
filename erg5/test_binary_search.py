fc5cda54d8058d3b2cb8b14762ff3e4add79cbc1
Lost <pandagaminggr@gmail.com>
2024-01-16 16:26:16 +0200

    chore: fix comment length

--- erg5/test_binary_search.py
+++ erg5/test_binary_search.py
@@ -5,17 +5,20 @@ def test_binary_search_empty_array():
     array = []
     assert binary_search(array, 1) == -1
 
-# Defines test function for binary_search with a sorted array and an element that exists in the array as input
+# Defines test function for binary_search with a sorted array
+# and an element that exists in the array as input
 def test_binary_search():
     array = [1, 2, 3, 4, 5]
     assert binary_search(array, 3) == 2
 
-# Defines test function for binary_search with a sorted array and an element that does not exist in the array as input
+# Defines test function for binary_search with a sorted array
+# and an element that does not exist in the array as input
 def test_binary_search_not_found():
     array = [1, 2, 3, 4, 5]
     assert binary_search(array, 6) == -1
 
-# Defines test function for binary_search with an unsorted array and an element that exists in the array as input
+# Defines test function for binary_search with an unsorted array
+# and an element that exists in the array as input
 def test_binary_search_unsorted_array():
     array = [3, 2, 5, 1, 4]
     assert binary_search(array, 3) == 2
