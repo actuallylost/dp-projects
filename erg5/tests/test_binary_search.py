70b27dfcfd7b6bff3c4b0a9d7143d53280ff4764
Lost <pandagaminggr@gmail.com>
2024-01-16 17:09:43 +0200

    test: add one-element array testing; make comments clearer

--- erg5/tests/test_binary_search.py
+++ erg5/tests/test_binary_search.py
@@ -5,25 +5,25 @@ def test_binary_search_empty_array():
     array = []
     assert binary_search(array, 1) == -1
 
-# Defines test function for binary_search with an array of length 1
+# Defines test function for binary_search with a one-element array
 # and an element that exists in the array as input
-def test_binary_search_length_1():
+def test_binary_search_one_element_array():
     array = [1]
     assert binary_search(array, 1) == 0
 
-# Defines test function for binary_search with a sorted array
+# Defines test function for binary_search with a sorted five-element array
 # and an element that exists in the array as input
 def test_binary_search():
     array = [1, 2, 3, 4, 5]
     assert binary_search(array, 3) == 2
 
-# Defines test function for binary_search with a sorted array
+# Defines test function for binary_search with a sorted five-element array
 # and an element that does not exist in the array as input
 def test_binary_search_not_found():
     array = [1, 2, 3, 4, 5]
     assert binary_search(array, 6) == -1
 
-# Defines test function for binary_search with an unsorted array
+# Defines test function for binary_search with an unsorted five-element array
 # and an element that exists in the array as input
 def test_binary_search_unsorted_array():
     array = [3, 2, 5, 1, 4]
